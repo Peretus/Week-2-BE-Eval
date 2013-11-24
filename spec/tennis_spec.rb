bce063f6eefa2eea04bf9e96628fd4cdbf2a0617
Peretus <caseymcneil@gmail.com>
2013-11-23 22:41:01 -0600

    Initial commit after formatting spec file.

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -4,69 +4,76 @@ require 'rspec'
 require_relative '../tennis'
 
 describe Tennis::Game do
-  let(:game) { Tennis::Game.new }
+    let(:game) { Tennis::Game.new }
+    # begin initialize test
+    describe '.initialize' do
+        it 'creates two players' do
+          expect(game.player1).to be_a(Tennis::Player)
+          expect(game.player2).to be_a(Tennis::Player)
+        end
+    
 
-  describe '.initialize' do
-    it 'creates two players' do
-      expect(game.player1).to be_a(Tennis::Player)
-      expect(game.player2).to be_a(Tennis::Player)
+
+        it 'sets the opponent for each player' do
+            expect(game.player1.opponent).to eq(game.player2)
+            expect(game.player2.opponent).to eq(game.player1)
+        end
+    end
+    
+    # begin wins_ball test
+    describe '#wins_ball' do
+        it 'increments the points of the winning player' do
+            game.wins_ball(1)
+            expect(game.player1.points).to eq(1)
+        end
     end
 
-    it 'sets the opponent for each player'
-  end
-
-  describe '#wins_ball' do
-    it 'increments the points of the winning player' do
-      game.wins_ball(1)
-
-      expect(game.player1.points).to eq(1)
-    end
-  end
 end
 
 describe Tennis::Player do
-  let(:player) do
-    player = Tennis::Player.new
-    player.opponent = Tennis::Player.new
+    let(:player) do
+        player = Tennis::Player.new
+        player.opponent = Tennis::Player.new
 
-    return player
-  end
-
-  describe '.initialize' do
-    it 'sets the points to 0' do
-      expect(player.points).to eq(0)
-    end 
-  end
-
-  describe '#record_won_ball!' do
-    it 'increments the points' do
-      player.record_won_ball!
-
-      expect(player.points).to eq(1)
+        return player
     end
-  end
 
-  describe '#score' do
-    context 'when points is 0' do
-      it 'returns love' do
-        expect(player.score).to eq('love')
-      end
+    describe '.initialize' do
+        it 'sets the points to 0' do
+            expect(player.points).to eq(0)
+        end 
     end
-    
-    context 'when points is 1' do
-      it 'returns fifteen' do
-        player.points = 1
 
-        expect(player.score).to eq('fifteen')
-      end 
+    describe '#record_won_ball!' do
+        it 'increments the points' do
+            player.record_won_ball!
+
+            expect(player.points).to eq(1)
+        end
     end
-    
-    context 'when points is 2' do
-      it 'returns thirty'  
+
+    describe '#score' do
+        context 'when points is 0' do
+            it 'returns love' do
+                expect(player.score).to eq('love')
+            end
+        end
+        
+        context 'when points is 1' do
+            it 'returns fifteen' do
+                player.points = 1
+                expect(player.score).to eq('fifteen')
+            end 
+        end
+        
+        context 'when points is 2' do
+            it 'returns thirty' do
+            end 
+        end
+        
+        context 'when points is 3' do
+            it 'returns forty' do
+            end
+        end
     end
-    
-    context 'when points is 3' do
-      it 'returns forty' 
-    end
-  end
 end
